3c666b3da56a0b2431827c7b8cffdb84d83f1f83
Shajiya-123 <shajiya786htce@gmail.com>
2026-02-05 11:50:31 +0530

    push the code

---
+++ Menu2.py
@@ -0,0 +1,23 @@
+
+a = int(input("please enter first number: "))
+b = int(input("please enter second number: "))
+    
+
+def menu():
+    print("\n~~~Menu~~~")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+
+    choice = int(input("\nEnter your choice: "))
+
+    if choice == 1:
+        print("Add =", a+b)
+    elif choice == 2:
+        print("Subtract =", a-b)
+    elif choice == 3:
+        print("Multiply =",a*b)
+    else:
+        print("invalid choice")
+
+menu()
